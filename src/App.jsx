d82f3ba8955881550723e6f4b6747fca10d2e909
Eduardo Santana <ezambomsantana@gmail.com>
2025-04-22 20:17:51 -0300

    add list

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,36 @@ function App() {
   const [descricao, setDescricao] = useState()
   const [tempo, setTempo] = useState()
 
+  const [musicas, setMusicas] = useState([])
+
   const {
     user,
     isAuthenticated,
     isLoading,
     getAccessTokenSilently
   } = useAuth0();
+
+  useEffect(() => {
+    if (token) {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      console.log('Email:', payload['https://musica-insper.com/email'])
+      console.log('Roles:', payload['https://musica-insper.com/roles'])
+
+      fetch('http://localhost:8080/musica', {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer ' + token
+        }
+      }).then(response => { 
+        return response.json()
+      }).then(data => { 
+        setMusicas(data)
+      }).catch(error => {
+        alert(error)
+      })
+    }
+
+  }, [token])
 
   useEffect(() => {
     const fetchToken = async () => {
@@ -29,15 +53,6 @@ function App() {
         console.error('Erro ao buscar token:', e);
       }
     };
-
-    try {
-      const payload = JSON.parse(atob(token.split('.')[1]));
-      console.log('Email:', payload['https://musica-insper.com/email']);
-      console.log('Roles:', payload['https://musica-insper.com/roles']);
-    } catch(e) {}
-    
-
-
 
     if (isAuthenticated) {
       fetchToken();
@@ -73,23 +88,42 @@ function App() {
 
   }
 
-  
-
   return (
     <>
       <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-        <h3>Token JWT:</h3>
-        <pre>{token}</pre>
-        <LogoutButton />
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+          <LogoutButton />
+        </div>
 
         <div>
           Título: <input type='text' onChange={e => setTitulo(e.target.value)} /><br/>
           Descrição: <input type='text' onChange={e => setDescricao(e.target.value)} /><br/>
           Tempo: <input type='text' onChange={e => setTempo(e.target.value)} /><br/>
           <button onClick={() => salvarMusica()}>Cadastrar</button>
+        </div>
+
+
+        <div>
+          <table>
+            <thead>
+              <th>Título</th>
+              <th>Descrição</th>
+              <th>Tempo</th>
+
+            </thead>
+            <tbody>
+                {musicas.map((musica, index) => {
+                return <tr key={index}>
+                      <td>{musica.titulo}</td>
+                      <td>{musica.descricao}</td>
+                      <td>{musica.tempo}</td>
+                    </tr>
+                })}
+            </tbody>
+          </table>
         </div>
       </div>
     </>
